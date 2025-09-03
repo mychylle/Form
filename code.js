4c620295ee1441fa9ea17aa8e9507b879dc52140
mychylle <164629252+mychylle@users.noreply.github.com>
2025-09-03 11:30:27 +0800

    Delete code.js

--- code.js
+++
@@ -1,9 +0,0 @@
-function doGet(e) {
-  return HtmlService.createHtmlOutputFromFile('index');
-}
-
-function saveData(data) {
-  var sheet = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet();
-  sheet.appendRow([data.name || '', data.score || '', new Date()]);
-  return 'ok';
-}
